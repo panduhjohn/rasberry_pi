7364346f0ccf80f8de6349768be8be02a13412ad
Daniel Lee <dan@pop-os.localdomain>
2020-11-16 16:08:14 -0500

    created a star rating, but still need to work on form

--- js/review.js
+++ js/review.js
@@ -1,4 +1,27 @@
+let starRating = 0;
+
 function deleteReviews(button) {
-    button.parentNode.parentNode.parentNode.remove()
+  button.parentNode.parentNode.parentNode.remove()
 }
 
+function highlight(integer) {
+  const stars = document.querySelectorAll('.star-rating');
+  for (let i = 0; i < integer; i++) {
+    stars[i].style.background = 'green';
+  }
+}
+
+function unhighlight() {
+  const stars = document.querySelectorAll('.star-rating');
+  stars.forEach(star => star.style.background = 'white')
+  highlight(starRating)
+}
+
+function saveStarRating(integer) {
+  console.log(integer, starRating)
+  starRating = integer
+  const stars = document.querySelectorAll('.star-rating');
+  for (let i = 0; i < integer; i++) {
+    stars[i].style.background = 'green';
+  }
+}
